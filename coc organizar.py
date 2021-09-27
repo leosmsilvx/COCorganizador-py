512cbd641db53964cf819f81e8f1bd183adbf7b7
Leonardo Silva <leosmsilvx@gmail.com>
2021-09-27 17:22:51 -0300

    Change the hole program to tkinter

--- coc organizar.py
+++ coc organizar.py
@@ -1,18 +1,54 @@
-numeros = int(input("Digite os numeros sorteados: "))
-lista = []
-#Separa o numero grande de 2 em 2 digitos
-while(numeros > 0):
-    #Separa os 2 ultimos digitos pelo resto do numero todo por 100
-    addlista = ((numeros) % 100)
-    #Retira os 2 ultimos numeros
-    numeros = int((numeros)//100)
-    #Adiciona a lista
-    lista.append(addlista)
-#Printa a lista de numeros
-print("Numeros não organizados: {}".format(lista))
-#Organiza em ordem crescente
-lista.sort(key=int)
-#Printa a lista em ordem crescente
-print("Numeros organizados: {}".format(lista))
-input("Digite qualquer coisa para fechar")        
+from tkinter import *
+import tkinter as tk
+#This program was made for "https://centrooestecap.com.br/resultados-cc/" to just ctrl+c and ctrl+v
+#And show to me the numbers organized
+#It was so simple so I made it using tkinter
+def main():
+    
+    def organize():
+        win = Tk()
+        
+        win.title("AutoPC")
+        win.geometry("765x150+100+100")
+        
+        lista = []
+        num = int(ed.get())
+        while(num > 0):
+            addlista = ((num) % 100)
+            num = int((num)//100)
+            lista.append(addlista)
+
+        lista.sort(key=int)
+        numbers = Label(win, text="Organized numbers:\n{}".format(str(lista)[1:-1])) # To remove the brackets
+        numbers.place(x=60,y=20)
+
+        bto = Button(win, width=10, text="Ok", command=win.destroy)
+        bto.place(x=334, y=90)
+
+        win.mainloop()
+        winm.destroy()
+    
+            
+    winm = Tk()
+
+    winm.title("AutoPC")
+    winm.geometry("425x150+100+100")
+    winm.iconphoto(True, tk.PhotoImage(file='icon.png'))
+
+
+    lb = Label(winm, text="Welcome to AutoPC\n\n\n\nType the numbers to organize")
+    lb.place(x=130,y=40)
+
+    ed = Entry(winm)
+    ed.place(x=110,y=70)
+    ed.focus_force()
+
+    bto = Button(winm, width=6, text="Ok", command=organize)
+    bto.place(x=240, y=67)
+
+    winm.mainloop()
+
+if __name__ == '__main__':
+    main()
+
         
